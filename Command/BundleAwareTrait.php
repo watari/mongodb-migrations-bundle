301ae4666c3292215cebcc9bb0e1b541865034a2
watari <watari.mailbox@gmail.com>
2018-06-28 18:43:55 +0300

    Updated logic for status command. Moved multi-bundle logic to BundleAwareTrait from migrate command.

--- Command/BundleAwareTrait.php
+++ Command/BundleAwareTrait.php
@@ -5,9 +5,12 @@ namespace AntiMattr\Bundle\MongoDBMigrationsBundle\Command;
 
 use AntiMattr\MongoDB\Migrations\Configuration\Configuration;
 use AntiMattr\MongoDB\Migrations\Configuration\Interfaces\ConfigurationInterface;
+use AntiMattr\MongoDB\Migrations\OutputWriter;
+use Doctrine\MongoDB\Connection;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 /**
  * Trait BundleAwareTrait
@@ -68,5 +71,87 @@ trait BundleAwareTrait
         }
 
         return  $this->configuration;
+    }
+
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return ConfigurationInterface[]
+     */
+    protected function getMigrationConfigurations(InputInterface $input, OutputInterface $output): array
+    {
+        $configs = [$this->getMigrationConfiguration($input, $output)];
+
+        $includeAllBundles = $input->getOption('include-bundles');
+        $bundleAliasesList = $input->getOption('include-bundle');
+
+        if (!empty($includeAllBundles) && !empty($bundleAliasesList)) {
+            throw new \InvalidArgumentException(
+                'Options "include-bundles" and "include-bundle" cannot be specified simultaneously.'
+            );
+        }
+
+        $outputWriter = new OutputWriter(
+            function ($message) use ($output) {
+                return $output->writeln($message);
+            }
+        );
+        $databaseConnection = $this->getDatabaseConnection($input);
+        $matchedBundles = [];
+
+        if ($includeAllBundles) {
+            /** @var array $registeredBundles */
+            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
+            /** @var BundleInterface[] $appBundles */
+            $appBundles = $this->container->get('kernel')->getBundles();
+
+            foreach ($appBundles as $bundle) {
+                $bundleExtension = $bundle->getContainerExtension();
+                if (null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])) {
+                    $matchedBundles[] = $bundle;
+                }
+            }
+        } elseif (!empty($bundleAliasesList)) {
+            /** @var array $registeredBundles */
+            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
+            /** @var BundleInterface[] $appBundles */
+            $appBundles = $this->container->get('kernel')->getBundles();
+            $bundleAliasesMap = \array_flip($bundleAliasesList);
+            foreach ($appBundles as $bundle) {
+                $bundleExtension = $bundle->getContainerExtension();
+                if (
+                    null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])
+                    && isset($bundleAliasesMap[$bundleExtension->getAlias()])
+                ) {
+                    $matchedBundles[] = $bundle;
+                }
+            }
+        }
+
+        foreach ($matchedBundles as $bundle) {
+            $configs[] = $this->createConfiguration(
+                $databaseConnection,
+                $outputWriter,
+                CommandHelper::getConfigParamsForBundle($this->container, $bundle)
+            );
+        }
+
+        return $configs;
+    }
+
+    protected function createConfiguration(
+        Connection $connection,
+        OutputWriter $outputWriter,
+        array $params
+    ): ConfigurationInterface {
+        $configuration = $this->getConfigurationBuilder()
+                              ->setConnection($connection)
+                              ->setOutputWriter($outputWriter)
+                              ->build();
+        CommandHelper::configureConfiguration($this->container, $params, $configuration);
+
+        return $configuration;
     }
 }
--- Command/MigrationsMigrateCommand.php
+++ Command/MigrationsMigrateCommand.php
@@ -12,22 +12,20 @@
 namespace AntiMattr\Bundle\MongoDBMigrationsBundle\Command;
 
 use AntiMattr\MongoDB\Migrations\Configuration\Configuration;
-use AntiMattr\MongoDB\Migrations\Configuration\Interfaces\ConfigurationInterface;
-use AntiMattr\MongoDB\Migrations\OutputWriter;
 use AntiMattr\MongoDB\Migrations\Tools\Console\Command\MigrateCommand;
-use Doctrine\MongoDB\Connection;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Matthew Fitzgerald <matthewfitz@gmail.com>
  */
 class MigrationsMigrateCommand extends MigrateCommand
 {
+    use BundleAwareTrait;
+
     protected $container;
 
 
@@ -53,6 +51,13 @@ class MigrationsMigrateCommand extends MigrateCommand
             null,
             InputOption::VALUE_NONE,
             'Indicate that all migrations should be applied.'
+        );
+        $this->addOption(
+            'bundle',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Alias of bundle for which migration will be generated.',
+            Configuration::DEFAULT_PREFIX
         );
         $this->addOption(
             'dm',
@@ -133,89 +138,5 @@ class MigrationsMigrateCommand extends MigrateCommand
         }
 
         return 0;
-    }
-
-    /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return ConfigurationInterface[]
-     */
-    protected function getMigrationConfigurations(InputInterface $input, OutputInterface $output): array
-    {
-        $configs = [
-            $this->getMigrationConfiguration($input, $output),
-        ];
-        CommandHelper::configureMigrations($this->container, $configs[0]);
-
-        $includeAllBundles = $input->getOption('include-bundles');
-        $bundleAliasesList = $input->getOption('include-bundle');
-
-        if (!empty($includeAllBundles) && !empty($bundleAliasesList)) {
-            throw new \InvalidArgumentException(
-                'Options "include-bundles" and "include-bundle" cannot be specified simultaneously.'
-            );
-        }
-
-        $outputWriter = new OutputWriter(
-            function ($message) use ($output) {
-                return $output->writeln($message);
-            }
-        );
-        $databaseConnection = $this->getDatabaseConnection($input);
-        $matchedBundles = [];
-
-        if ($includeAllBundles) {
-            /** @var array $registeredBundles */
-            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
-            /** @var Bundle[] $appBundles */
-            $appBundles = $this->container->get('kernel')->getBundles();
-
-            foreach ($appBundles as $bundle) {
-                $bundleExtension = $bundle->getContainerExtension();
-                if (null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])) {
-                    $matchedBundles[] = $bundle;
-                }
-            }
-        } elseif (!empty($bundleAliasesList)) {
-            /** @var array $registeredBundles */
-            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
-            /** @var Bundle[] $appBundles */
-            $appBundles = $this->container->get('kernel')->getBundles();
-            $bundleAliasesMap = \array_flip($bundleAliasesList);
-            foreach ($appBundles as $bundle) {
-                $bundleExtension = $bundle->getContainerExtension();
-                if (
-                    null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])
-                    && isset($bundleAliasesMap[$bundleExtension->getAlias()])
-                ) {
-                    $matchedBundles[] = $bundle;
-                }
-            }
-        }
-
-        foreach ($matchedBundles as $bundle) {
-            $configs[] = $this->createConfiguration(
-                $databaseConnection,
-                $outputWriter,
-                CommandHelper::getConfigParamsForBundle($this->container, $bundle)
-            );
-        }
-
-        return $configs;
-    }
-
-    protected function createConfiguration(
-        Connection $connection,
-        OutputWriter $outputWriter,
-        array $params
-    ): ConfigurationInterface {
-        $configuration = $this->getConfigurationBuilder()
-                              ->setConnection($connection)
-                              ->setOutputWriter($outputWriter)
-                              ->build();
-        CommandHelper::configureConfiguration($this->container, $params, $configuration);
-
-        return $configuration;
     }
 }
