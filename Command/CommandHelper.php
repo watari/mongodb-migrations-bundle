2c086124a083671f6e5d14161bc7d8065e00674e
watari <watari.mailbox@gmail.com>
2018-06-28 16:34:52 +0300

    Updated logic for migrate command.

--- Command/CommandHelper.php
+++ Command/CommandHelper.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /*
  * This file is part of the AntiMattr MongoDB Migrations Bundle, a library by Matthew Fitzgerald.
@@ -12,11 +13,13 @@
 namespace AntiMattr\Bundle\MongoDBMigrationsBundle\Command;
 
 use AntiMattr\Bundle\MongoDBMigrationsBundle\Configuration\Configuration;
+use AntiMattr\MongoDB\Migrations\Configuration\Interfaces\ConfigurationInterface;
 use Doctrine\ODM\MongoDB\Tools\Console\Helper\DocumentManagerHelper;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 /**
  * @author Matthew Fitzgerald <matthewfitz@gmail.com>
@@ -29,13 +32,22 @@ final class CommandHelper
      * @param ContainerInterface $container
      * @param Configuration      $configuration
      */
-    public static function configureMigrations(ContainerInterface $container, Configuration $configuration)
+    public static function configureMigrations(ContainerInterface $container, ConfigurationInterface $configuration)
     {
         $params = self::getConfigParams($container, $configuration);
-        if (!file_exists($params['dir_name'])) {
-            mkdir($params['dir_name'], 0777, true);
+        self::configureConfiguration($container, $params, $configuration);
+    }
+
+    public static function configureConfiguration(
+        ContainerInterface $container,
+        array $params,
+        ConfigurationInterface $configuration
+    ): void {
+        if (!\file_exists($params['dir_name'])) {
+            \mkdir($params['dir_name'], 0777, true);
         }
 
+        $configuration->setPrefix($params['prefix']);
         $configuration->setMigrationsCollectionName($params['collection_name']);
         $configuration->setMigrationsDatabaseName($params['database_name']);
         $configuration->setMigrationsDirectory($params['dir_name']);
@@ -63,7 +75,7 @@ final class CommandHelper
         $helperSet->set(new DocumentManagerHelper($dm), 'dm');
     }
 
-    protected static function getConfigParams(ContainerInterface $container, Configuration $configuration): array
+    protected static function getConfigParams(ContainerInterface $container, ConfigurationInterface $configuration): array
     {
         $params = [
             'collection_name' => $container->getParameter('mongo_db_migrations.collection_name'),
@@ -71,7 +83,7 @@ final class CommandHelper
             'script_dir_name' => $container->getParameter('mongo_db_migrations.script_dir_name'),
         ];
 
-        if (!empty($configuration->getMigrationsBundleAlias())) {
+        if ($configuration instanceof Configuration && !empty($configuration->getMigrationsBundleAlias())) {
             $bundleAlias = $configuration->getMigrationsBundleAlias();
             $bundles = $container->getParameter('mongo_db_migrations.bundles');
             if (empty($bundles[$bundleAlias])) {
@@ -83,7 +95,7 @@ final class CommandHelper
             $targetBundle = null;
             foreach ($bundles as $bundle) {
                 $containerExtension = $bundle->getContainerExtension();
-                if(null !== $containerExtension && $containerExtension->getAlias() === $bundleAlias) {
+                if (null !== $containerExtension && $containerExtension->getAlias() === $bundleAlias) {
                     $targetBundle = $bundle;
                     break;
                 }
@@ -95,13 +107,43 @@ final class CommandHelper
             $params['namespace'] = $targetBundle->getNamespace() . '\\' . $bundleConfig['namespace'];
             $params['dir_name'] = $targetBundle->getPath() . '/' . $bundleConfig['dir_name'];
             $params['name'] = $bundleConfig['name'];
+            $params['prefix'] = $bundleAlias;
         } else {
+            $params['prefix'] = 'app';
             $params['name'] = $container->getParameter('mongo_db_migrations.name');
             $params['namespace'] = $container->getParameter('mongo_db_migrations.namespace');
             $params['dir_name'] = $container->getParameter('mongo_db_migrations.dir_name');
         }
 
         return $params;
+    }
+
+    public static function getConfigParamsForBundle(
+        ContainerInterface $container,
+        BundleInterface $bundle
+    ): array {
+        if ($bundle->getContainerExtension() == null) {
+            throw new \InvalidArgumentException(
+                "Bundle with name {$bundle->getName()} do not have bundle extension. Bundle alias cannot be defined."
+            );
+        }
+        $bundleAlias = $bundle->getContainerExtension()->getAlias();
+        $bundleConfigs = $container->getParameter('mongo_db_migrations.bundles');
+        if (empty($bundleConfigs[$bundleAlias])) {
+            throw new \RuntimeException("Bundle with alias {$bundleAlias} has no registered migration configs");
+        }
+
+        $bundleConfig = $bundleConfigs[$bundleAlias];
+
+        return [
+            'collection_name' => $container->getParameter('mongo_db_migrations.collection_name'),
+            'database_name' => $container->getParameter('mongo_db_migrations.database_name'),
+            'script_dir_name' => $container->getParameter('mongo_db_migrations.script_dir_name'),
+            'namespace' => $bundle->getNamespace() . '\\' . $bundleConfig['namespace'],
+            'dir_name' => $bundle->getPath() . '/' . $bundleConfig['dir_name'],
+            'name' => $bundleConfig['name'],
+            'prefix' => $bundleAlias,
+        ];
     }
 
     /**
--- Command/MigrationsMigrateCommand.php
+++ Command/MigrationsMigrateCommand.php
@@ -11,33 +11,211 @@
 
 namespace AntiMattr\Bundle\MongoDBMigrationsBundle\Command;
 
+use AntiMattr\MongoDB\Migrations\Configuration\Configuration;
+use AntiMattr\MongoDB\Migrations\Configuration\Interfaces\ConfigurationInterface;
+use AntiMattr\MongoDB\Migrations\OutputWriter;
 use AntiMattr\MongoDB\Migrations\Tools\Console\Command\MigrateCommand;
+use Doctrine\MongoDB\Connection;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Matthew Fitzgerald <matthewfitz@gmail.com>
  */
 class MigrationsMigrateCommand extends MigrateCommand
 {
+    protected $container;
+
+
+    public function __construct(?string $name = null, ContainerInterface $container)
+    {
+        parent::__construct($name);
+        $this->container = $container;
+    }
+
     protected function configure(): void
     {
         parent::configure();
 
-        $this
-            ->setName('mongodb:migrations:migrate')
-            ->addOption('dm', null, InputOption::VALUE_OPTIONAL, 'The document manager to use for this command.', 'default_document_manager')
-        ;
+        $this->setName('mongodb:migrations:migrate');
+        $this->addOption(
+            'include-bundle',
+            null,
+            InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+            'Alias of bundle which migrations should be applied.'
+        );
+        $this->addOption(
+            'include-bundles',
+            null,
+            InputOption::VALUE_NONE,
+            'Indicate that all migrations should be applied.'
+        );
+        $this->addOption(
+            'dm',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'The document manager to use for this command.',
+            'default_document_manager'
+        );
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         CommandHelper::setApplicationDocumentManager($this->getApplication(), $input->getOption('dm'));
 
-        $configuration = $this->getMigrationConfiguration($input, $output);
-        CommandHelper::configureMigrations($this->getApplication()->getKernel()->getContainer(), $configuration);
+        $version = $input->getArgument('version');
+        $configurations = $this->getMigrationConfigurations($input, $output);
 
-        parent::execute($input, $output);
+        $isInteractive = $input->isInteractive();
+
+
+        // warn the user if no dry run and interaction is on
+        if ($isInteractive) {
+            $question = new ConfirmationQuestion(
+                '<question>WARNING! You are about to execute a database migration that could result in data lost. Are you sure you wish to continue? (y/[n])</question> ',
+                false
+            );
+
+            $confirmation = $this->getHelper('question')->ask($input, $output, $question);
+
+            if (!$confirmation) {
+                $output->writeln('<error>Migration cancelled!</error>');
+
+                return 1;
+            }
+        }
+
+        foreach ($configurations as $configuration) {
+
+            $migration = $this->createMigration($configuration);
+            $this->outputHeader($configuration, $output);
+
+            $executedVersions = $configuration->getMigratedVersions();
+            $availableVersions = $configuration->getAvailableVersions();
+            $executedUnavailableVersions = array_diff($executedVersions, $availableVersions);
+
+            if (!empty($executedUnavailableVersions)) {
+                $output->writeln(sprintf('<error>WARNING! You have %s previously executed migrations in the database that are not registered migrations.</error>',
+                    count($executedUnavailableVersions)));
+                foreach ($executedUnavailableVersions as $executedUnavailableVersion) {
+                    $output->writeln(
+                        sprintf(
+                            '    <comment>>></comment> %s (<comment>%s</comment>)',
+                            Configuration::formatVersion($executedUnavailableVersion),
+                            $executedUnavailableVersion
+                        )
+                    );
+                }
+
+                if ($isInteractive) {
+                    $question = new ConfirmationQuestion(
+                        '<question>Are you sure you wish to continue? (y/[n])</question> ',
+                        false
+                    );
+
+                    $confirmation = $this
+                        ->getHelper('question')
+                        ->ask($input, $output, $question);
+
+                    if (!$confirmation) {
+                        $output->writeln('<error>Migration cancelled!</error>');
+
+                        return 1;
+                    }
+                }
+            }
+
+            $migration->migrate($version);
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return ConfigurationInterface[]
+     */
+    protected function getMigrationConfigurations(InputInterface $input, OutputInterface $output): array
+    {
+        $configs = [
+            $this->getMigrationConfiguration($input, $output),
+        ];
+        CommandHelper::configureMigrations($this->container, $configs[0]);
+
+        $includeAllBundles = $input->getOption('include-bundles');
+        $bundleAliasesList = $input->getOption('include-bundle');
+
+        if (!empty($includeAllBundles) && !empty($bundleAliasesList)) {
+            throw new \InvalidArgumentException(
+                'Options "include-bundles" and "include-bundle" cannot be specified simultaneously.'
+            );
+        }
+
+        $outputWriter = new OutputWriter(
+            function ($message) use ($output) {
+                return $output->writeln($message);
+            }
+        );
+        $databaseConnection = $this->getDatabaseConnection($input);
+        $matchedBundles = [];
+
+        if ($includeAllBundles) {
+            /** @var array $registeredBundles */
+            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
+            /** @var Bundle[] $appBundles */
+            $appBundles = $this->container->get('kernel')->getBundles();
+
+            foreach ($appBundles as $bundle) {
+                $bundleExtension = $bundle->getContainerExtension();
+                if (null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])) {
+                    $matchedBundles[] = $bundle;
+                }
+            }
+        } elseif (!empty($bundleAliasesList)) {
+            /** @var array $registeredBundles */
+            $registeredBundles = $this->container->getParameter('mongo_db_migrations.bundles');
+            /** @var Bundle[] $appBundles */
+            $appBundles = $this->container->get('kernel')->getBundles();
+            $bundleAliasesMap = \array_flip($bundleAliasesList);
+            foreach ($appBundles as $bundle) {
+                $bundleExtension = $bundle->getContainerExtension();
+                if (
+                    null !== $bundleExtension && !empty($registeredBundles[$bundleExtension->getAlias()])
+                    && !empty($bundleAliasesMap[$bundleExtension->getAlias()])
+                ) {
+                    $matchedBundles[] = $bundle;
+                }
+            }
+        }
+
+        foreach ($matchedBundles as $bundle) {
+            $configs[] = $this->createConfiguration(
+                $databaseConnection,
+                $outputWriter,
+                CommandHelper::getConfigParamsForBundle($this->container, $bundle)
+            );
+        }
+
+        return $configs;
+    }
+
+    protected function createConfiguration(
+        Connection $connection,
+        OutputWriter $outputWriter,
+        array $params
+    ): ConfigurationInterface {
+        $configuration = $this->getConfigurationBuilder()
+                              ->setConnection($connection)
+                              ->setOutputWriter($outputWriter)
+                              ->build();
+        CommandHelper::configureConfiguration($this->container, $params, $configuration);
+
+        return $configuration;
     }
 }
